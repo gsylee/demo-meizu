f4a30b245e4a63713b9d7b6d512518b51ebeec65
gsyan <gsylee@163.com>
2018-05-05 16:29:01 +0800

    使用组件形式重写了index.js

--- demo-meizu/js/index.js
+++ demo-meizu/js/index.js
@@ -1,177 +1,161 @@
-/* 
- * cloasAd: 实现效果： 点击关闭广告按钮，获取header这个设置了绝对定位的元素
- *           然后每隔10毫秒就修改一次定位的top值，当top值为0的时候，
- *          就表明广告被完全覆盖了，然后让这个广告消失
- */
-addLoadEvent(closeAd);
-function closeAd() {
-    // 按钮
-    var close_btn = byId("close-btn");
-    // id为header的元素
-    var header = byId("header");
-    // id为top-ad的元素
-    var top_ad = byId("top-ad");
-    var timer = null; //保存定时器的返回值
-    var top = 0; //保存定位的top值
-    // 给按钮绑定点击事件
-    close_btn.onclick = function () {
-        // 获取header元素的top值
-        timer = setInterval(function () {
-            top = parseInt(header .style.marginTop,10);
-            // 判断top是否为0；
-            if(top === 0) {
-                // 清除定时器
-                clearInterval(timer);
-                // 让头部的广告消失
-                top_ad.style.display = "none";
+// 命名空间：减少代码冲突
+var INDEX = {
+    // 关闭广告
+    closeAd: function closeAd() {
+        // 按钮
+        var close_btn = byId("close-btn");
+        // id为header的元素
+        var header = byId("header");
+        // id为top-ad的元素
+        var top_ad = byId("top-ad");
+        var timer = null; //保存定时器的返回值
+        var top = 0; //保存定位的top值
+        // 给按钮绑定点击事件
+        close_btn.onclick = function () {
+            // 获取header元素的top值
+            timer = setInterval(function () {
+                top = parseInt(header .style.marginTop,10);
+                // 判断top是否为0；
+                if(top === 0) {
+                    // 清除定时器
+                    clearInterval(timer);
+                    // 让头部的广告消失
+                    top_ad.style.display = "none";
+                }
+                header.style.marginTop  = top - 2 + "px";
+            },10);
+        };
+    },
+
+    // 输入框
+    fnInput: function fnInput() {
+        $(".form input").focus(function() {
+            $(this).val("");
+        }).blur(function() {
+            $(this).val(this.defaultValue);
+        });
+    },
+    
+    // 使用jquery实现轮播图效果
+    marquee: function marquee() {
+        var $btns = $("#mq-btn span");
+        var $as = $("#marquee a");
+        var myIndex = 0;
+        var timer = null; //保存定时器的返回值
+        var time = 2000; //定时器执行间隔事件
+        $btns.hover(function() {
+            // 清除定时器
+            clearInterval(timer);
+            var v = parseInt($(this).attr("order"));
+            if(myIndex == v ) {
+                return false;
+            } else {
+                myIndex = v;
+                mqCon(myIndex);
             }
-            header.style.marginTop  = top - 2 + "px";
-        },10);
-    };
-}
-
-// 输入框
-addLoadEvent(fnInput);
-function fnInput() {
-    $(".form input").focus(function() {
-        $(this).val("");
-    }).blur(function() {
-        $(this).val(this.defaultValue);
-    });
-}
-
-// 使用jquery实现轮播图效果
-addLoadEvent(marquee);
-function marquee() {
-    var $btns = $("#mq-btn span");
-    var $as = $("#marquee a");
-    var myIndex = 0;
-    var timer = null; //保存定时器的返回值
-    var time = 2000; //定时器执行间隔事件
-    $btns.hover(function() {
-        // 清除定时器
-        clearInterval(timer);
-        var v = parseInt($(this).attr("order"));
-        if(myIndex == v ) {
-            return false;
-        } else {
-            myIndex = v;
-            mqCon(myIndex);
-        }
-    },function() {
+        },function() {
+            timer = setInterval(autoPlay,time);
+        });
         timer = setInterval(autoPlay,time);
-    });
-    timer = setInterval(autoPlay,time);
-    // 定时器回调函数
-    function autoPlay() {
-        myIndex ++;
-        if(myIndex > 4) {
-            myIndex = 0;
+        // 定时器回调函数
+        function autoPlay() {
+            myIndex ++;
+            if(myIndex > 4) {
+                myIndex = 0;
+            }
+           mqCon(myIndex);
         }
-       mqCon(myIndex);
-    }
-
-    // 鼠标放在图片上的事件
-    $as.hover(function() {
-        clearInterval(timer);
-    },function() {
-        timer = setInterval(autoPlay,time);
-    });
-
-    // 公共代码
-    function mqCon(curindex){
-        // 按钮高亮效果
-        $btns.eq(curindex)
-             .addClass("high-color")
-             .siblings().removeClass("high-color");
-        // 图片切换效果
-        $as.eq(curindex)
-           .stop(true).fadeIn("500")
-           .siblings().fadeOut("500");
-    }
-}
-
-// 选项卡左右切换
-addLoadEvent(tagChange);
-function tagChange() {
-    // 获取包围元素的宽度
-    var con_wrap = $("#con-wrap").width();
-    var index = 1; //表示当前所在的页数
-    // 获取总得宽度
-    var $con = $("#con");
-    var con_width = $con.width();
-    // console.log(con_width);
-    var all_page = Math.ceil(con_width / con_wrap);
-    // 给prev和next 元素添加点击事件
-    var $next = $("#next");
-    var $prev = $("#prev");
-    $next.click(function() {
-       if(index < all_page) {
-             index += 1 ;
-           $con.stop(false,true)
-               .animate({"left": "-=" + con_wrap + "px"},"normal");
-            // if(index == all_page) {
-            //     $(this).css("background","url(/images/index/icon/no-right.png) no-repeat center");
-            // }
-       }else {
-           return false;
-       }
-    });
-    $prev.click(function() {
-        if(index > 1) {
-            index -= 1;
-            $con.stop(false,true)
-                .animate({"left": "+=" + con_wrap + "px"},"normal");
-            // if(index == 1) {
-            //     $(this).css("background","url(/images/index/icon/no-left.png) no-repeat center");
-            // }
-        }else {
-            return false;
+    
+        // 鼠标放在图片上的事件
+        $as.hover(function() {
+            clearInterval(timer);
+        },function() {
+            timer = setInterval(autoPlay,time);
+        });
+    
+        // 公共代码
+        function mqCon(curindex){
+            // 按钮高亮效果
+            $btns.eq(curindex)
+                 .addClass("high-color")
+                 .siblings().removeClass("high-color");
+            // 图片切换效果
+            $as.eq(curindex)
+               .stop(true).fadeIn("500")
+               .siblings().fadeOut("500");
         }
-    });
-}
+    },
 
-/* 
- * lazyLoad():  图片的懒加载 
- *              需要获取： 浏览器的可视区域
- *                        滚动条的滚动距离
- *                        图片距离页面顶部的距离
- */
+    // 左右切换效果：
+    lrChange: function lrChange() {
+        // 获取包围元素的宽度
+        var con_wrap = $("#con-wrap").width();
+        var index = 1; //表示当前所在的页数
+        // 获取总得宽度
+        var $con = $("#con");
+        var con_width = $con.width();
+        // console.log(con_width);
+        var all_page = Math.ceil(con_width / con_wrap);
+        // 给prev和next 元素添加点击事件
+        var $next = $("#next");
+        var $prev = $("#prev");
+        $next.click(function() {
+           if(index < all_page) {
+                 index += 1 ;
+               $con.stop(false,true)
+                   .animate({"left": "-=" + con_wrap + "px"},"normal");
+                // if(index == all_page) {
+                //     $(this).css("background","url(/images/index/icon/no-right.png) no-repeat center");
+                // }
+           }else {
+               return false;
+           }
+        });
+        $prev.click(function() {
+            if(index > 1) {
+                index -= 1;
+                $con.stop(false,true)
+                    .animate({"left": "+=" + con_wrap + "px"},"normal");
+                // if(index == 1) {
+                //     $(this).css("background","url(/images/index/icon/no-left.png) no-repeat center");
+                // }
+            }else {
+                return false;
+            }
+        });
+    },
 
-
-
-
-// 手机周边： 选项卡切换效果
-addLoadEvent(tgChange);
-function tgChange() {
-    // 获取要绑定事件的元素
-    var $ul = $('#tagchange');
-    var $as = $ul.find('a');
-    // 给每个li添加一个order序号
-
-    // $as.each( function(i) {
-    //     $(this).attr('order',i);
-    // });
-    for(var i =0,len=$as.length; i< len; i++) {
-        $as[i].setAttribute('order',i);
-    }
-    var order = 0; //初始化保存鼠标经过的元素的order值
-    // 获取 要进行切换的内容
-    var $tagcons = $('#tag-con ul');
-    // 通过on()方法，吧事件绑定在三个选项卡的父元素上面
-    $ul.on('mouseenter','a',function() {
-        // 判断此时鼠标经过的a元素是不是当前高亮的a元素
-        var cindex = $as.index(this);
-        if(cindex == order) {
-            return false;
-        }else {
-            $(this).addClass('high')
-            .parent().siblings()
-            .children('a').removeClass('high');
-           $tagcons.eq(cindex).show()
-            .siblings().hide();
-            order = $(this).attr('order');
+    // 手机周边，选项卡切换效果
+    tgChange: function tgChange() {
+        // 获取要绑定事件的元素
+        var $ul = $('#tagchange');
+        var $as = $ul.find('a');
+        // 给每个li添加一个order序号
+    
+        // $as.each( function(i) {
+        //     $(this).attr('order',i);
+        // });
+        for(var i =0,len=$as.length; i< len; i++) {
+            $as[i].setAttribute('order',i);
         }
-    });
-}
+        var order = 0; //初始化保存鼠标经过的元素的order值
+        // 获取 要进行切换的内容
+        var $tagcons = $('#tag-con ul');
+        // 通过on()方法，吧事件绑定在三个选项卡的父元素上面
+        $ul.on('mouseenter','a',function() {
+            // 判断此时鼠标经过的a元素是不是当前高亮的a元素
+            var cindex = $as.index(this);
+            if(cindex == order) {
+                return false;
+            }else {
+                $(this).addClass('high')
+                .parent().siblings()
+                .children('a').removeClass('high');
+               $tagcons.eq(cindex).show()
+                .siblings().hide();
+                order = $(this).attr('order');
+            }
+        });
+    }
+};
 
--- demo-meizu/js/min/common.js
+++ demo-meizu/js/min/common.js
@@ -3,19 +3,19 @@
  * addloadEvent: 同时给window。onload添加事件
  * @param {func} : 在window.onload时执行的时间
  */
-function addLoadEvent (func) {
-    var old_onload = window.onload;
-    if(typeof old_onload !== "function") {
-        window.onload = function() {
-            func();
-        };
-    }else {
-        window.onload = function () {
-            old_onload();
-            func()
-        };
-    }
-}
+// function addLoadEvent (func) {
+//     var old_onload = window.onload;
+//     if(typeof old_onload !== "function") {
+//         window.onload = function() {
+//             func();
+//         };
+//     }else {
+//         window.onload = function () {
+//             old_onload();
+//             func()
+//         };
+//     }
+// }
 
 /* 
  * byId : 通过document.getElementById 获取对应的元素
@@ -34,3 +34,4 @@ function byId(idname) {
     }
     return em_obj;
 }
+
